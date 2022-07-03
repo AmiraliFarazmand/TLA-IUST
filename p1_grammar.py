4ae5872cad0115d8be9ecda7daf003bdd664169b
amirfzd <amiralifarazmand80@gmail.com>
2022-07-03 17:44:52 +0330

    grammar class created

---
+++ p1_grammar.py
@@ -0,0 +1,47 @@
+'''
+Determine whether the given grammar accepts the input or not.
+instances:
+3
+<S> -> a<S>b | a<A> | b<B>
+<A> -> a<A> | #
+<B> -> b<B> | #
+aaab
+    >>>Accepted
+------------------------------------------------------------------------------
+4
+<S> -> <S>a | <S>b | <A>a | <B>b
+<A> -> ab<A> | <B>ca | #
+<B> -> b<B> | <C>f
+<C> -> a<C> | #
+abbfcaba
+    >>>Rejected
+'''
+import time 
+class Grammar():
+    def __init__(self,var , products ):
+    # var -> products
+        self.var = var
+        self.products = products    
+    def apply_grammar(self,input):
+        input = input.replace(self.var,self.products)
+    @classmethod
+    def does_accept(cls,input,string):
+        if input.replace(cls.var,cls.products)==string:
+            print('Accepted')
+            exit()
+
+
+
+if __name__ == '__main__':
+    grammars_dict = {}
+    n=int(input())
+    for i in range(n):
+        x , y = input().split('->')
+        x=x.replace(' ','')
+        y=y.replace(' ','')
+        y_list = y.split('|')
+        for i in y_list:
+            temp_grammar = Grammar(x , i)
+            grammars_dict[x]= temp_grammar
+            
+input_str = input()

c47b66d4383207600095f7e886dc22dc5c559ead
amirfzd <amiralifarazmand80@gmail.com>
2022-07-04 02:30:26 +0330

    grammar class and 2 functions added - need to debug!!!

--- p1_grammar.py
+++ p1_grammar.py
@@ -28,21 +28,21 @@ class Grammar():
             input_str = input_str.replace(self.var,'')
         else:
             input_str = input_str.replace(self.var,self.products)
-    @classmethod
-    def does_accept(cls,input_var,string):
+    
+def does_accept(cls,input_var,string):
         if input_var.replace(cls.var,cls.products)==string:
             print('Accepted')
             exit()
 
-def derivate(gr:Grammar , string:str):
-    string = string.replace(gr.var,gr.products)
-    Grammar.does_accept(gr,string)    
-    g_list= re.findall('^<.$>' , string)
+def derivate(gr:Grammar , inc_string:str ,tmp_string:str='' ):
+    tmp_string = tmp_string.replace(gr.var,gr.products)
+    does_accept(gr,tmp_string,inc_string)    
+    g_list= re.findall('^<.$>' , tmp_string)
     for g in g_list:
-        for i in grammar_list:
+        for i in grammars_list:
             if i[0] == g:
                 gram = i[1]
-                derivate(gram,string) 
+                derivate(gram,inc_string,tmp_string) 
     
 
 if __name__ == '__main__':
@@ -58,7 +58,8 @@ if __name__ == '__main__':
             grammars_list.append([x ,  temp_grammar ])
             
     input_str = input()
-
-    derivate(grammars_list[0][1] , '')
+    time0=time.time()
+    derivate(grammars_list[0][1] , input_str ,'') 
+    time.sleep(1)
+    print('Rejected')
     # print(grammars_list[5][1].products)
-    

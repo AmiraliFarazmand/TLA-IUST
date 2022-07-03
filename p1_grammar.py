3860896dc23beeee6884276168e03e1cf1df9c45
amirfzd <amiralifarazmand80@gmail.com>
2022-07-04 00:40:22 +0330

    derivate function doesnt work properly

--- p1_grammar.py
+++ p1_grammar.py
@@ -16,27 +16,37 @@ aaab
 abbfcaba
     >>>Rejected
 '''
+import re
 import time 
 class Grammar():
     def __init__(self,var , products ):
     # var -> products
         self.var = var
         self.products = products    
-    def apply_grammar(self,input):
+    def apply_grammar(self,input_str):
         if self.products=='#':
-            input = input.replace(self.var,'')
+            input_str = input_str.replace(self.var,'')
         else:
-            input = input.replace(self.var,self.products)
+            input_str = input_str.replace(self.var,self.products)
     @classmethod
-    def does_accept(cls,input,string):
-        if input.replace(cls.var,cls.products)==string:
+    def does_accept(cls,input_var,string):
+        if input_var.replace(cls.var,cls.products)==string:
             print('Accepted')
             exit()
 
-def 
+def derivate(gr:Grammar , string:str):
+    string = string.replace(gr.var,gr.products)
+    Grammar.does_accept(gr,string)    
+    g_list= re.findall('^<.$>' , string)
+    for g in g_list:
+        for i in grammar_list:
+            if i[0] == g:
+                gram = i[1]
+                derivate(gram,string) 
+    
 
 if __name__ == '__main__':
-    grammars_dict = {}
+    grammars_list =[]
     n=int(input())
     for i in range(n):
         x , y = input().split('->')
@@ -45,8 +55,10 @@ if __name__ == '__main__':
         y_list = y.split('|')
         for i in y_list:
             temp_grammar = Grammar(x , i)
-            grammars_dict[x]= temp_grammar
+            grammars_list.append([x ,  temp_grammar ])
             
     input_str = input()
 
+    derivate(grammars_list[0][1] , '')
+    # print(grammars_list[5][1].products)
     

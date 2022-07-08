25b779d0a4865c836050493168b11b0a0ee6f07b
amirfzd <amiralifarazmand80@gmail.com>
2022-07-08 20:30:12 +0330

    p1:more instances added + need work on step()

--- p1_grammar.py
+++ p1_grammar.py
@@ -15,6 +15,27 @@ aaab
 <C> -> a<C> | #
 abbfcaba
     >>>Rejected
+------------------------------------------------------------------------------
+4
+<E> -> <E>+<T> | <E>-<T> | <T>
+<T> -> <T>*<F> | <T>/<F> | <F>
+<F> -> <F><G> | <G>
+<G> -> 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+42+56/8125442
+-----------------------------------------------------------------------------
+4
+<E> -> <E>+<T> | <E>-<T> | <T>
+<T> -> <T>*<F> | <T>/<F> | <F>
+<F> -> <F><G> | <G>
+<G> -> 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+30+-71111
+------------------------------------------------------------------------------
+4
+<E> -> <E>+<T> | <E>-<T> | <T>
+<T> -> <T>*<F> | <T>/<F> | <F>
+<F> -> <F><G> | <G>
+<G> -> 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+15*9
 '''
 
 import re
@@ -41,18 +62,27 @@ def derivate(gr:Grammar , inc_string:str ,tmp_string:str='' ):
         # tmp_string = tmp_string.replace(gr.var,gr.products)
         g_list= re.findall(r'<[A-Za-z0-9]*>', tmp_string)
         # if tmp_string==inc_string: print('***didnt work',tmp_string,g_list)
-        if len(tmp_string)<=len(inc_string)+5 and g_list!=[]:
+        if len(tmp_string)<=3*len(inc_string):
             for g in g_list:
                 for i in grammars_list:
                     if i[0] == g:
-                        gram = i[1]
-                        derivate(gram,inc_string,tmp_string) 
+                        if list([i[1], tmp_string]) not in visited_situations:
+                            if '<' in i[1].products:
+                                gram = i[1]
+                                print('*****' ,tmp_string , ';' ,gram.var , '->' ,gram.products)
+                                derivate(gram,inc_string,tmp_string) 
+                                visited_situations.append(list([i[1], tmp_string]))
+                            else:
+                                gram = i[1]
+                                derivate(gram,inc_string,tmp_string) 
+                                visited_situations.append(list([i[1], tmp_string]))
         else:
             pass
         
 
 if __name__ == '__main__':
     grammars_list =[]
+    visited_situations = [] #prevents grom getting into loops
     n=int(input())
     for i in range(n):
         x , y = input().split('->')
@@ -66,10 +96,13 @@ if __name__ == '__main__':
     input_str = input()
     time0=time.time()
 
-    for g in grammars_list:
+    for g in grammars_list :
+        # if time.time()<= time0+1.5:
         # for iv in grammars_list:
-        derivate(g[1] , input_str , grammars_list[0][0])
-    time.sleep(9.5)
+            # derivate(g[1] , input_str , iv[0])
+            derivate(g[1] , input_str , grammars_list[0][0])
+    
+    # time.sleep(0.5)
     print('Rejected')
-
+    # print(*visited_situations ,sep='\n')
     
